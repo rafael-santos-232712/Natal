73bbabbc9e4b224436c930ed86ea3872819196ef
Rafael Rodrigues <146786161+rafael-santos-232712@users.noreply.github.com>
2023-12-07 14:54:39 +0000

    Foi instalado a biblioteca para uso do bootstrap

---
+++ Frontend/vue-project/src/main.js
@@ -0,0 +1,11 @@
+
+
+import BootstrapVueNext from 'bootstrap-vue-next'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+const app = createApp(App)
+
+app.use(BootstrapVueNext)
