311a183c5a46175d05b40aeda9e46d7f7bfbda6d
Rafael Rodrigues <146786161+rafael-santos-232712@users.noreply.github.com>
2023-12-07 16:46:14 +0000

    Criado a tela Doar

---
+++ Frontend/vue-project/src/components/Doar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Doar</h1>
+    <!-- Conteúdo da página de doação -->
+  </div>
+</template>
+
+<script>
+export default {
+  // Lógica do componente
+};
+</script>
+
+<style>
+/* Estilos da página de doação */
+</style>
