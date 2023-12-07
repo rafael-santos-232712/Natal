4a1a4a8d58e179af6031de7a979790c611af2a73
Rafael Rodrigues <146786161+rafael-santos-232712@users.noreply.github.com>
2023-12-07 16:13:48 +0000

    Foi alterado codigo, retirando o import (doar/ receber)

--- Frontend/vue-project/src/router/index.js
+++ Frontend/vue-project/src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Doar from '../views/Doar.vue'
-import Receber from '../views/Receber.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
