bcdfefbbe204734378b21565fd25e97a3714f3c8
Rafael Rodrigues <146786161+rafael-santos-232712@users.noreply.github.com>
2024-01-06 19:10:27 +0000

    Update gift migration

--- Backend/natal_solidario/db/migrate/20240106171720_create_gifts.rb
+++ Backend/natal_solidario/db/migrate/20240106171720_create_gifts.rb
@@ -1,7 +1,7 @@
 class CreateGifts < ActiveRecord::Migration[7.1]
   def change
     create_table :gifts do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :description
       t.references :user, null: false, foreign_key: true
 
